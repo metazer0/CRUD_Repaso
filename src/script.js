09ef26ac8f29385a4bbec945b99277ef4218b131
Julio David <isaacdvid15.96@outlook.com>
2023-07-05 20:39:24 -0600

    update

---
+++ src/script.js
@@ -0,0 +1,15 @@
+function searchById(){
+    this.location.href = "search-user.html"
+}
+
+function addUser(){
+    this.location.href = "add-user.html"
+}
+
+function editUser(){
+    this.location.href = "edit-user.html"
+}
+
+function deleteUser(){
+    this.location.href = "delete-user.html"
+}
